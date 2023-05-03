3da7f241d13621177b3fe3c9ed574b1635f47232
Clifton Gockul <clifga@gmail.com>
2023-05-03 16:26:30 -0400

    completed

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, condition = 'Used', size = 0 ):
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self,size):
+        if type(size) != int:
+            print('size must be an integer')
+            return False
+        self._size  = size
+    
+    def cobble(self):
+        self.condition = 'New'
+        print('Your shoe is as good as new!')
+
+    size = property(get_size, set_size)        
